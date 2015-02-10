a9f2ae209c5ffe7ca1a48681213a5ba4d298b879
Yuan Zhencai <zhencai.yuan@sunlights.cc>
2015-02-10 19:11:18 +0800

    find menus

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,28 +1,49 @@
-angular.module('starter.controllers', [])
+angular.module('order.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+    .controller('DashCtrl', function ($scope) {
+    })
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  }
-})
+    .controller('ChatsCtrl', function ($scope, Chats) {
+        $scope.chats = Chats.all();
+        $scope.remove = function (chat) {
+            Chats.remove(chat);
+        }
+    })
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
+    .controller('ChatDetailCtrl', function ($scope, $stateParams, Chats) {
+        $scope.chat = Chats.get($stateParams.chatId);
+    })
 
-.controller('FriendsCtrl', function($scope, Friends) {
-  $scope.friends = Friends.all();
-})
+    .controller('FriendsCtrl', function ($scope, Friends) {
+        $scope.friends = Friends.all();
+    })
 
-.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
-  $scope.friend = Friends.get($stateParams.friendId);
-})
+    .controller('FriendDetailCtrl', function ($scope, $stateParams, Friends) {
+        $scope.friend = Friends.get($stateParams.friendId);
+    })
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-});
+    .controller('AccountCtrl', function ($scope) {
+        $scope.settings = {
+            enableFriends: true
+        };
+    })
+
+    .controller('MenuCtrl', function ($scope, $log, MenuService) {
+        $scope.menus = {};
+        $scope.error = {};
+        $scope.pager = {};
+
+        $scope.findMenus = function () {
+            $log.debug("findMenus()");
+            return MenuService.findMenus($scope.pager).then((function (data) {
+                console.debug("Promise returned " + data.value.list.length + " menus");
+                $scope.menus = data.value.list;
+
+            }), function (error) {
+                console.error("Unable to find menus: " + error);
+                $scope.error = error;
+            });
+        }
+    })
+
+;
